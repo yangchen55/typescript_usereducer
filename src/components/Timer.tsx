e713e14c441fee751689d7d1661fd27b829f4fa8
Tsering yangchen <33425130+yangchen55@users.noreply.github.com>
2024-01-06 19:33:18 +1100

    second commit

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,42 @@
 import Container from "./UI/Container.tsx";
-import { type Timer as TimerProps } from "./store/timers-context.tsx";
+import { useState, useEffect, useRef } from "react";
+import {
+  useTimerContext,
+  type Timer as TimerProps,
+} from "./store/timers-context.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimerContext();
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    let timer: number;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prevTime) => prevTime - 50);
+      }, 50);
+      interval.current = timer;
+    } else if (!isRunning && interval.current) {
+      clearInterval(interval.current);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning]);
+  // empty arry got no dependency, code only run once even after rerender and reexecute
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+        djkh
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
